0f97f021613249e3563369ca73efae8a1ed3759d
Juderton <juderton@gmail.com>
2023-05-29 17:48:06 -0400

    Add bot.py and admin cog

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+token = open('token.txt').readline()
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    await bot.change_presence(
+        status=discord.Status.do_not_disturb,
+        activity=discord.Activity(type=discord.ActivityType.listening,
+                                  name='commands'))
+    print('Changed presence')
+
+    for file in os.scandir('cogs'):
+        if file.name.endswith('.py'):
+            await bot.load_extension(f'cogs.{file.name[:-3]}')
+
+    print('Loaded extensions from cogs')
+    print('Syncing commands...')
+    await bot.tree.sync()
+    print('Synced commands')
+    print(f'{bot.user} is ready!')
+
+bot.run(token)
---
+++ cogs/admin.py
@@ -0,0 +1,38 @@
+from discord.ext import commands
+from discord import app_commands, Interaction
+from typing import Literal
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(description='Reloads an extension')
+    async def reload(self,
+                     interaction: Interaction,
+                     extension: Literal['admin'],
+                     sync_commands: bool):
+
+        if interaction.user.id == 688832593570955354:
+            content = f'Reloading `{extension}` extension'
+            await interaction.response.send_message(content=content)
+            await self.bot.reload_extension(f'cogs.{extension}')
+            content = f'{content}\nDone'
+            await interaction.edit_original_response(content=content)
+
+            if sync_commands:
+                content = f'{content}\nSyncing commands...'
+                await interaction.edit_original_response(
+                    content=content)
+                await self.bot.tree.sync()
+                content = f'{content}\nDone'
+                await interaction.edit_original_response(
+                    content=content)
+        else:
+            await interaction.response.send_message(
+                content='You do not have permission to execute this command',
+                ephemeral=True)
+
+
+async def setup(bot):
+    await bot.add_cog(Admin(bot))
